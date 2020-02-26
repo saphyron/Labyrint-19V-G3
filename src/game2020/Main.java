201b7f540cabbaa251cfe3a89ab849b566f64295
John Grandt Markvard Høeg <saphyron@hotmail.com>
2020-02-26 10:05:41 +0100

    Update Main.java

--- src/game2020/Main.java
+++ src/game2020/Main.java
@@ -125,13 +125,17 @@ public class Main extends Application {
 			
             // Setting up standard players
 			
-			me = new Player("Orville",9,4,"up");
+			me = new Player("Jeppe",9,4,"up");
 			players.add(me);
 			fields[9][4].setGraphic(new ImageView(hero_up));
 
-			Player harry = new Player("Harry",14,15,"up");
-			players.add(harry);
+			Player John = new Player("John",14,15,"up");
+			players.add(John);
 			fields[14][15].setGraphic(new ImageView(hero_up));
+			
+			Player Joakim = new Player("Joakim", 14, 4, "up");
+			players.add(Joakim);
+			fields[14][4].setGraphic(new ImageView(hero_up));
 
 			scoreList.setText(getScoreList());
 		} catch(Exception e) {
