dbbb607d4da667dd304115a3b98f8151a77315b3
pseudoguru <dan@DMC006.dataminds.network>
2020-03-05 18:53:33 +0100

    works now

--- src/game2020/Main.java
+++ src/game2020/Main.java
@@ -1,6 +1,9 @@
 package game2020;
 
+import java.io.DataOutputStream;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 
 import javafx.application.Application;
@@ -15,7 +18,9 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.*;
 
 public class Main extends Application {
-
+	
+	public static Socket clientSocket;
+	
 	public static final int size = 20; 
 	public static final int scene_height = size * 20 + 100;
 	public static final int scene_width = size * 20 + 200;
@@ -60,94 +65,108 @@ public class Main extends Application {
 	// | boardGrid (0,1)          | scorelist    |
 	// |                          | (1,1)        |
 	// -------------------------------------------
-
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			GridPane grid = new GridPane();
-			grid.setHgap(10);
-			grid.setVgap(10);
-			grid.setPadding(new Insets(0, 10, 0, 10));
-
-			Text mazeLabel = new Text("Maze:");
-			mazeLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 	
-			Text scoreLabel = new Text("Score:");
-			scoreLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+	public void initStart(Stage primaryStage) throws Exception {
 
-			scoreList = new TextArea();
-			
-			GridPane boardGrid = new GridPane();
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(0, 10, 0, 10));
 
-			image_wall  = new Image(getClass().getResourceAsStream("Image/wall4.png"),size,size,false,false);
-			image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"),size,size,false,false);
+		Text mazeLabel = new Text("Maze:");
+		mazeLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
-			hero_right  = new Image(getClass().getResourceAsStream("Image/heroRight.png"),size,size,false,false);
-			hero_left   = new Image(getClass().getResourceAsStream("Image/heroLeft.png"),size,size,false,false);
-			hero_up     = new Image(getClass().getResourceAsStream("Image/heroUp.png"),size,size,false,false);
-			hero_down   = new Image(getClass().getResourceAsStream("Image/heroDown.png"),size,size,false,false);
+		Text scoreLabel = new Text("Score:");
+		scoreLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
-			fields = new Label[20][20];
-			for (int j=0; j<20; j++) {
-				for (int i=0; i<20; i++) {
-					switch (board[j].charAt(i)) {
-					case 'w':
-						fields[i][j] = new Label("", new ImageView(image_wall));
-						break;
-					case ' ':					
-						fields[i][j] = new Label("", new ImageView(image_floor));
-						break;
-					default: throw new Exception("Illegal field value: "+board[j].charAt(i) );
-					}
-					boardGrid.add(fields[i][j], i, j);
+		scoreList = new TextArea();
+		
+		GridPane boardGrid = new GridPane();
+
+		image_wall  = new Image(getClass().getResourceAsStream("Image/wall4.png"),size,size,false,false);
+		image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"),size,size,false,false);
+
+		hero_right  = new Image(getClass().getResourceAsStream("Image/heroRight.png"),size,size,false,false);
+		hero_left   = new Image(getClass().getResourceAsStream("Image/heroLeft.png"),size,size,false,false);
+		hero_up     = new Image(getClass().getResourceAsStream("Image/heroUp.png"),size,size,false,false);
+		hero_down   = new Image(getClass().getResourceAsStream("Image/heroDown.png"),size,size,false,false);
+
+		fields = new Label[20][20];
+		for (int j=0; j<20; j++) {
+			for (int i=0; i<20; i++) {
+				switch (board[j].charAt(i)) {
+				case 'w':
+					fields[i][j] = new Label("", new ImageView(image_wall));
+					break;
+				case ' ':					
+					fields[i][j] = new Label("", new ImageView(image_floor));
+					break;
+				default: throw new Exception("Illegal field value: "+board[j].charAt(i) );
 				}
+				boardGrid.add(fields[i][j], i, j);
 			}
-			scoreList.setEditable(false);
-			
-			
-			grid.add(mazeLabel,  0, 0); 
-			grid.add(scoreLabel, 1, 0); 
-			grid.add(boardGrid,  0, 1);
-			grid.add(scoreList,  1, 1);
-						
-			Scene scene = new Scene(grid,scene_width,scene_height);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-
-			scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
-				switch (event.getCode()) {
-				case UP:    playerMoved(0,-1,"up");    break;
-				case DOWN:  playerMoved(0,+1,"down");  break;
-				case LEFT:  playerMoved(-1,0,"left");  break;
-				case RIGHT: playerMoved(+1,0,"right"); break;
-				default: break;
-				}
-			});
-			
-            // Setting up standard players
-			
-			me = new Player("Jeppe",9,4,"up");
-			players.add(me);
-			fields[9][4].setGraphic(new ImageView(hero_up));
-
-			Player John = new Player("John",14,15,"up");
-			players.add(John);
-			fields[14][15].setGraphic(new ImageView(hero_up));
-			
-			Player Joakim = new Player("Joakim", 14, 4, "up");
-			players.add(Joakim);
-			fields[14][4].setGraphic(new ImageView(hero_up));
-
-			scoreList.setText(getScoreList());
-		} catch(Exception e) {
-			e.printStackTrace();
 		}
+		scoreList.setEditable(false);
+		
+		
+		grid.add(mazeLabel,  0, 0); 
+		grid.add(scoreLabel, 1, 0); 
+		grid.add(boardGrid,  0, 1);
+		grid.add(scoreList,  1, 1);
+					
+		Scene scene = new Scene(grid,scene_width,scene_height);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+			switch (event.getCode()) {
+			case UP:    playerMoved(0,-1,"up");    break;
+			case DOWN:  playerMoved(0,+1,"down");  break;
+			case LEFT:  playerMoved(-1,0,"left");  break;
+			case RIGHT: playerMoved(+1,0,"right"); break;
+			default: break;
+			}
+		});
+		
+        // Setting up standard players
+
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+			initStart(primaryStage);
+			scoreList.setText(getScoreList());
+			
+			var clientSocket = MsgFromServer.clientSocket;
+			Player player = new Player("kim", 6, 7, "up");
+			players.add(player);
+			
+			if (clientSocket != null) {
+				var outToServer = new DataOutputStream(clientSocket.getOutputStream());
+				outToServer.writeBytes("hello");
+			} else {
+				System.out.println("client socket is null");
+			}
+	}
+	
+	public static Hashtable<String, Player> playersHash = new Hashtable<String, Player>();
+	
+	public static void spawnPlayer(String name, int x, int y) {
+		
+		System.out.println("a new player shall be spawned");
+		Platform.runLater(() -> {
+			System.out.println(name + " " + x + " " + y + " up");
+			Player player = new Player(name, x, y, "up");
+			players.add(player);
+			fields[9][4].setGraphic(new ImageView(hero_up));
+			//scoreList.setText(getScoreList());
+		});
 	}
 	
 	public static void playerMovedFromServer(String direction) {
-		//System.out.println("hi");
+
 		Platform.runLater(() -> {
-			
 			switch (direction) {
 			case "UP":    playerMoved(0,-1,"up");    break;
 			case "DOWN":  playerMoved(0,+1,"down");  break;
@@ -215,8 +234,11 @@ public class Main extends Application {
 	}
 
 	public static void main(String[] args) {
-		var a = new MsgFromServer();
-		a.start();
+		
+		
+		MsgFromServer msgFromServer = new MsgFromServer();
+		msgFromServer.start();
+		clientSocket = msgFromServer.clientSocket;
 		launch(args);
 	}
 }
--- src/game2020/MsgFromServer.java
+++ src/game2020/MsgFromServer.java
@@ -4,40 +4,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class MsgFromServer extends Thread{
+public class MsgFromServer extends Thread {
 	
-	
-	
-	public void run()  {
-		
-		
-		Socket clientSocket;
-		
+	public static Socket clientSocket;
+	public void run() {
+
+		String msgFromServer = "";
+		List<String> formattedMsgFromServer = new ArrayList<>();
+
 		try {
-			clientSocket = new Socket("localhost", 6789);	
+			clientSocket = new Socket("localhost", 6789);
 		} catch (Exception e) {
+			System.err.println("could not connect to server");
 			return;
 		}
-		while (true) {
-			
-			String modifiedSentence;
-			
-			try {
+		try {
+			while (true) {
 				BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-				modifiedSentence = inFromServer.readLine();
-				System.out.println("FROM server:¤" + modifiedSentence + "¤");
-				Main.playerMovedFromServer(modifiedSentence);
-				//Main.playerMoved(0,-1,"up");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				msgFromServer = inFromServer.readLine();
+				System.out.println("FROM server: " + msgFromServer);
+				
+				formattedMsgFromServer = Arrays.asList(msgFromServer.split(" ")); 
+				
+				System.out.println(formattedMsgFromServer);
+				
+				if (formattedMsgFromServer.get(0).equals("spawn")) {
+					Main.spawnPlayer(formattedMsgFromServer.get(1), 6, 7);
+				}
 			}
-		
-			
+		} catch (IOException e) {
+			System.out.println("IO Exception from server");
 		}
-		
-		
-		
+
 	}
 }
